986f1cc5fb899017f101fb932f35da80a35c5f0e
freddysilber <freddy.silber@gmail.com>
2020-05-12 13:42:44 -0600

    adds app.js comment

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # portfolio-site
+
 My personal portfolio website to showcase my skills and learn about me as an individual
+
--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  // hosts the application
   return (
     <div className="App">
       <header className="App-header">
