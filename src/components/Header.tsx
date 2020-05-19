cf360e63349c499c24ef144c7f8d1c8c8f17f6a9
freddysilber <freddy.silber@gmail.com>
2020-05-18 22:12:30 -0600

    typescript setup

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+// import React from 'react'
+
+// export const Header = () => <div>Header</div>
+
+// export default Header
+
+import * as React from "react";
+
+export interface HelloProps { compiler: string; framework: string; }
+
+export const Hello = (props: HelloProps) => <h1>Hello from {props.compiler} and {props.framework}!</h1>;
--- src/components/LinkedInBadge.js
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-const LinkedInBadge = () => <div className="LI-profile-badge" data-version="v1" data-size="medium" data-locale="en_US" data-type="horizontal" data-theme="dark" data-vanity="freddysilber"><a className="LI-simple-link" href='https://www.linkedin.com/in/freddysilber?trk=profile-badge'>Freddy Silber</a></div>
-
-export default LinkedInBadge
