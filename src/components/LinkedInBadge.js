2fef38c431bcb6ad7bea679541d63b415c856a76
freddysilber <freddy.silber@gmail.com>
2020-05-18 21:44:26 -0600

    Revert "Revert "adds linkedin badge component""
    This reverts commit 125f79280808046329df0216fd37e1c993361079.

---
+++ src/components/LinkedInBadge.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const LinkedInBadge = () => <div class="LI-profile-badge" data-version="v1" data-size="medium" data-locale="en_US" data-type="horizontal" data-theme="dark" data-vanity="freddysilber"><a class="LI-simple-link" href='https://www.linkedin.com/in/freddysilber?trk=profile-badge'>Freddy Silber</a></div>
+
+export default LinkedInBadge
