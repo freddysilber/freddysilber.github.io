037bdf281e90990ca87c1b23dc474a68810e3a45
freddysilber <freddy.silber@gmail.com>
2020-05-20 17:22:12 -0600

    refactor

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,21 +1,19 @@
 import React from 'react'
-import { AppBar, Toolbar, Typography } from '@material-ui/core'
+import { AppBar, Toolbar, Typography, IconButton, } from '@material-ui/core'
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles'
+import MenuIcon from '@material-ui/icons/Menu'
 
 const useStyles = makeStyles((theme: Theme) =>
 	createStyles({
-		root: {
-			flexGrow: 1,
-		},
 		menuButton: {
-			marginRight: theme.spacing(2),
+			marginRight: theme.spacing(2)
 		},
-		title: {
-			flexGrow: 1,
+		buttonIcon: {
+			float: 'right'
 		},
 		background: {
 			background: '#666'
-		}
+		},
 	}),
 )
 
@@ -24,6 +22,9 @@ export default function Header() {
 	return (
 		<AppBar className={classes.background} position="static">
 			<Toolbar variant="dense">
+				<IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+					<MenuIcon className={classes.buttonIcon} />
+				</IconButton>
 				<Typography variant="h4"><p className="headerText">Freddy Silber / Software Engineer</p></Typography>
 			</Toolbar>
 		</AppBar>
