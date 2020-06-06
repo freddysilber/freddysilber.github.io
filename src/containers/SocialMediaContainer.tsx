891fa18eac352ac8da82a2ee54f825c08a3dad75
freddysilber <freddy.silber@gmail.com>
2020-06-06 15:57:03 -0600

    build

--- src/containers/SocialMediaContainer.tsx
+++ src/containers/SocialMediaContainer.tsx
@@ -1,14 +1,16 @@
 import React from 'react'
+import { Paper } from '@material-ui/core'
 import { LinkedInBadge, TwitterFollow } from '../components'
 
 export default class SocialMediaContainer extends React.Component {
 	render() {
+		// const classes = useStyles()
 		return (
-			<section>
-				<h1>Check me out on my social media!</h1>
+			<Paper elevation={3} style={{ background: '#666' }}>
+				<h3>Check me out on my social media!</h3>
 				<TwitterFollow />
 				<LinkedInBadge />
-			</section>
+			</Paper>
 		)
 	}
 }
