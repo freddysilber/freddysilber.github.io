ca920129a463d316c8211becced1c2d893484c04
freddysilber <freddy.silber@gmail.com>
2020-12-29 16:49:37 -0700

    adding navigation tabs

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,18 @@ import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 import './App.scss'
-import { Home } from './components'
+import { Home, Navigation } from './components'
 import { Routes } from './util'
 
 export default function App() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path={Routes.home} component={Home} />
-      </Switch>
-    </Router>
+    <>
+      <Navigation />
+      <Router>
+        <Switch>
+          <Route exact path={Routes.home} component={Home} />
+        </Switch>
+      </Router>
+    </>
   )
 }
--- src/components/index.ts
+++ src/components/index.ts
@@ -3,6 +3,7 @@ export { default as Header } from './Header'
 export { default as Bio } from './Bio'
 export { default as Skills } from './Skills'
 export { default as Home } from './Home'
+export { default as Navigation } from './Navigation'
 // Social Media Components
 export { default as LinkedInBadge } from './socialmedia/LinkedInBadge'
 export { default as TwitterFollow } from './socialmedia/TwitterFollow'
