53153f88b94334a55d2a2d2b1922490b0f2c0447
freddysilber <freddy.silber@gmail.com>
2020-12-29 23:53:58 -0700

    bio update

--- src/components/Bio.tsx
+++ src/components/Bio.tsx
@@ -34,7 +34,7 @@ export default function Bio(): JSX.Element {
 				<p>
 					I started snowboarding when I was 4 years old and would average almost 100 days a season snowboarding at my local ski area, {winterParkResortUrl()}.
 				I graduated {middleParkHighSchool()} in Granby Colorado and during school, I would often find myself nerding out in our computer lab
-				with some of my friends. We would do all kinds of things from creating claymation films, 3D printing, programming 3D printers, video games, and our attemps to hack other computer programs.
+				with some of my friends. We would do all kinds of things like creating claymation films, 3D printing, programming 3D printers, video games, and our attemps to hack other computer programs.
 			</p>
 				<p>
 					Shortly after I graduated high school, I landed a job with a company called {cloudcoachurl()} and started learning basic programming skills on the {salesforceUrl()}.com CRM platform.
@@ -43,8 +43,8 @@ export default function Bio(): JSX.Element {
 			</p>
 				<p>
 					I have recently graduated from {flatironSchoolUrl()} for the Full Stack Software Engineering curriculum. During this course, I have practiced skills
-				within the entire stack of a web application. I got to learn languages like Ruby, Ruby on Rails, Javascript, React, Redux and many more as well as
-				building over 5 projects to add to my portfolio! I loved every part of the school and learning from amazing people.
+				within the entire stack of a web application. I learned many languages, some of witch are Ruby, Ruby on Rails, Javascript, React, Redux, Angular, Node and many more as well as
+				building multiple projects to add to my portfolio!
 			</p>
 				<p>
 					Asside from programming all the time, I like to play electric guitar, cook, play with my cat, skateboard, snowboard, bmx bike, mountain bike, fish, hike, camp and much much more... oh, and keep learning!
