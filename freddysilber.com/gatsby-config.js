b03850ae13e8c9e6a9e1de772db61f664fdf0540
freddysilber <freddy.silber@gmail.com>
2020-12-30 13:49:44 -0700

    assets

--- freddysilber.com/gatsby-config.js
+++ freddysilber.com/gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Freddy Silber /// Software Engineer`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
--- freddysilber.com/src/components/header/Header.tsx
+++ freddysilber.com/src/components/header/Header.tsx
@@ -1,15 +1,17 @@
 import { Link } from 'gatsby'
 import PropTypes from 'prop-types'
 import React from 'react'
+// Utilities
+import { Routes } from '../../util'
 // Styles
 import './index.scss'
 
 const Header = ({ siteTitle }) => (
-	<header style={{ background: `rebeccapurple`, marginBottom: `1.45rem` }}>
+	<header style={{ background: '#666', marginBottom: `1.45rem` }}>
 		<div style={{ margin: `0 auto`, maxWidth: 960, padding: `1.45rem 1.0875rem` }}>
-			<h1 style={{ margin: 0 }}>
-				<Link to="/" style={{ color: `white`, textDecoration: `none` }}>
-					{siteTitle} - SITE TITLE
+			<h1>
+				<Link to={Routes.home} className='header'>
+					{siteTitle}
 				</Link>
 			</h1>
 		</div>
