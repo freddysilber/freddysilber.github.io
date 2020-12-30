8f9de9d058522a0febf70d029caf64f6ece99068
freddysilber <freddy.silber@gmail.com>
2020-12-30 00:31:56 -0700

    comments

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
-
+// Components
 import { Skills, Bio } from '../components'
+// Containers
 import { SocialMediaContainer } from '../containers'
 
 export default function Home(): JSX.Element {
