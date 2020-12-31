903d37c9b6ce5ef31c9e16fbe100a4f83e2144b2
freddysilber <freddy.silber@gmail.com>
2020-12-31 13:01:42 -0700

    removes stuffs

--- src/util/imageQueries.ts
+++
@@ -1,9 +0,0 @@
-
-export const imageQueries = {
-	horseRide: `file(relativePath: { eq: "images/default.jpg" }) {
-		childImageSharp {
-			fluid(maxWidth: 400) {
-		...GatsbyImageSharpFluid
-			}
-	}`
-}}
--- src/util/index.ts
+++ src/util/index.ts
@@ -1,3 +1,2 @@
 export { Routes } from './routes'
 export { urls } from './urls'
-export * from './queries'
