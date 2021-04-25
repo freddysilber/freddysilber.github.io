74b0a47d15d681a37eb37d84882a096bdf31d52b
freddysilber <freddy.silber@gmail.com>
2021-04-25 15:44:53 -0600

    npm cleanup

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,10 +7,10 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-sass`,
-    `gatsby-plugin-twitter`,
     `gatsby-plugin-react-helmet`,
-    `gatsby-transformer-sharp`,
+    `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
