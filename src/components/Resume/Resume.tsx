6de8909b0f45b5ff80432a6eebf5e2e3f8003532
freddysilber <freddy.silber@gmail.com>
2021-04-28 20:47:41 -0600

    starting resume and stylesheet

--- src/components/Resume/Resume.tsx
+++ src/components/Resume/Resume.tsx
@@ -6,10 +6,10 @@ export default function Resume() {
 	return (
 		<div className="container">
 			<div className="resumeContainer">
-				<h1 className="titleName">Freddy Silber</h1>
+				<h1 className="bold">Freddy Silber</h1>
 				<p>970-531-0297 | freddy.silber@gmail.com</p>
 				<p>LinkedIn | Github | Blog | Portfolio | YouTube</p>
-				<h1 className="centerText marginTop">Software Engineer</h1>
+				<h1 className="centerText marginTop bold">Software Engineer</h1>
 				<hr />
 				<p>
 					Experienced in Ruby on Rails, Java, Apex and JavaScript based programming.
@@ -18,13 +18,15 @@ export default function Resume() {
 					and project management skills to help high tech and software companies meet their business goals on an efficient timeline.
 					Work well with peers and clients to gather information in a friendly manner to meet deadlines. Avid snowboarder who loves being outdoors in the mountains.
 				</p>
-				<h1 className="centerText marginTop">Technical Skills</h1>
+				<h1 className="centerText marginTop bold">Technical Skills</h1>
 				<hr />
 				<p>
 					Angular, Salesforce, Ruby, Ruby on Rails, SQL, Javascript, React, Redux, Redux Thunk, HTML5, CSS, Rake, Apex, Database Triggers,
 					Automation/ Process Automation, Java, Lightning Components, Visualforce Pages, Git, Github, reCharts, D3.js, MatLab,
 					Project Management, B2B Solutions, Salesforce, Adobe Illustrator, Microsoft Excel, Final Cut Pro, Bootstrap
 				</p>
+				<h1 className="centerText marginTop bold">Technical Projects</h1>
+				<hr />
 			</div>
 		</div>
 	)
