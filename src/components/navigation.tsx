c835412944d07286d86604353d46d0bd747f990f
freddysilber <freddy.silber@gmail.com>
2021-05-12 20:55:14 -0600

    blog navi

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -9,7 +9,7 @@ export default function Navigation(): JSX.Element {
 			<div className="navbar-menu">
 				<div className="navbar-start">
 					<Link className="navbar-item" to={Routes.home} activeClassName="active">Home</Link>
-					<Link className="navbar-item" to={Routes.blog} activeClassName="active">Blog</Link>
+					<Link className="navbar-item" to={Routes.blog} partiallyActive={true} activeClassName="active">Blog</Link>
 					<Link className="navbar-item" to={Routes.contact} activeClassName="active">Contact</Link>
 					<Link className="navbar-item" to={Routes.projects} activeClassName="active">Projects</Link>
 					<Link className="navbar-item" to={Routes.resume} activeClassName="active">Resume</Link>
--- src/pages/{MarkdownRemark.frontmatter__slug}.tsx
+++ src/pages/{MarkdownRemark.frontmatter__slug}.tsx
@@ -1,15 +1,7 @@
 import React from 'react'
 import { graphql, Link } from 'gatsby'
+import { Layout } from '../components';
 import { Routes } from '../util';
-
-const styles = {
-	blogPostContentContainer: {
-		padding: '1rem',
-	},
-	navContainer: {
-		margin: '0.5rem',
-	},
-};
 
 interface MarkdownData {
 	data: {
@@ -30,25 +22,16 @@ export default function Template({
 	const { markdownRemark } = data // data.markdownRemark holds your post data
 	const { frontmatter, html } = markdownRemark
 	return (
-		<div className="blog-post-container">
-			<div className="blog-post">
-				<nav className="navbar is-dark" style={styles.navContainer} role="navigation" aria-label="main navigation">
-					<div className="navbar-menu">
-						<div className="navbar-start">
-							<Link className="navbar-item" to={Routes.blog} activeClassName="active">Back</Link>
-							<Link className="navbar-item" to={Routes.blog} activeClassName="active">Previous</Link>
-							<Link className="navbar-item" to={Routes.blog} activeClassName="active">Next</Link>
-						</div>
-					</div>
-				</nav>
-				<hr style={{ margin: '0' }} />
-				<div style={styles.blogPostContentContainer}>
+		<Layout>
+			<div className="blog-post-container">
+				<Link to={Routes.blog}>Back</Link>
+				<div className="blog-post">
 					<h1>{frontmatter.title}</h1>
 					<h2>{frontmatter.date}</h2>
 					<div dangerouslySetInnerHTML={{ __html: html }} />
 				</div>
-			</div >
-		</div >
+			</div>
+		</Layout>
 	)
 }
 
