97435f1a89bc73a9eea74ad0e353274c7af5ad58
freddysilber <freddy.silber@gmail.com>
2021-05-12 20:18:24 -0600

    reorders skills

--- src/components/skills.tsx
+++ src/components/skills.tsx
@@ -2,18 +2,18 @@ import React from 'react'
 
 const ICONS: string[] = [
 	'devicon-react-original',
+	'devicon-angularjs-plain',
 	'devicon-html5-plain',
 	'devicon-css3-plain',
+	'devicon-typescript-plain',
 	'devicon-git-plain',
 	'devicon-java-plain',
 	'devicon-mysql-plain',
 	'devicon-postgresql-plain',
 	'devicon-ruby-plain',
 	'devicon-sass-original',
-	'devicon-typescript-plain',
 	'devicon-visualstudio-plain',
 	'devicon-webpack-plain',
-	'devicon-angularjs-plain'
 ]
 
 
