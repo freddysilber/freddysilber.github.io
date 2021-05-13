98da557b5c8223cd6ea19da99d427366cad91ad8
freddysilber <freddy.silber@gmail.com>
2021-05-12 20:57:13 -0600

    removes stuff

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 import { Layout, SEO } from '../components'
 // Hooks
 // import { useSiteMetadata } from '../hooks'
-import { StaticImage } from "gatsby-plugin-image"
+// import { StaticImage } from "gatsby-plugin-image"
 import { Link } from 'gatsby'
 
 export default function SecondPage() {
@@ -13,9 +13,8 @@ export default function SecondPage() {
 		<Layout>
 			<SEO title="Blog" />
 			{/* <h1>welcome to {title}</h1> */}
-			<h1>Coming soon... so heres another picture</h1>
 			<Link to="/blog/my-first-post/">First Post</Link>
-			<StaticImage style={{ width: '20%' }} src="../assets/images/horseRide.png" alt="Horse Ride" />
+			{/* <StaticImage style={{ width: '20%' }} src="../assets/images/horseRide.png" alt="Horse Ride" /> */}
 		</Layout >
 	)
 }
