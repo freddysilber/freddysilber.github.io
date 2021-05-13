59d2d0c4e300f78f5e7cbac20030f31b35877619
freddysilber <freddy.silber@gmail.com>
2021-05-12 20:15:57 -0600

    indentation

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -13,16 +13,16 @@ import { useStaticQuery, graphql } from 'gatsby'
 function SEO({ description, lang, meta, title }: any) {
 	const { site } = useStaticQuery(
 		graphql`
-      query {
-        site {
-          siteMetadata {
-            title
-            description
-            author
-          }
-        }
-      }
-    `
+			query {
+				site {
+					siteMetadata {
+						title
+						description
+						author
+					}
+				}
+			}
+		`
 	)
 
 	const metaDescription = description || site.siteMetadata.description
