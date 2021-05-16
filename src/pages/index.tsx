e26115baf8f2fd7f657e9012da9ce73627618cf8
Freddy Silber <freddy.silber@cloudcoach.com>
2021-05-16 17:00:38 -0600

    responsive image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { StaticImage } from 'gatsby-plugin-image'
+import { graphql, useStaticQuery } from 'gatsby'
+import Img from 'gatsby-image'
 import styled from 'styled-components'
 // Components
 import {
@@ -14,21 +15,37 @@ const IndexContainer = styled.section`
 	display: flex;
 `;
 
+const AvatarWrapper = styled.div`
+	width: 100%;
+	height: auto;
+	margin: auto;
+
+	.gatsby-image-wrapper {
+		border-radius: 100%;
+		box-shadow: 0 5px 5px rgba(0, 0, 0, 0.5);
+	}
+`
+
 export default function IndexPage() {
+	const data = useStaticQuery(graphql`
+		query {
+			placeholderImage: file(relativePath: { eq: "avatar.jpg" }) {
+				childImageSharp {
+					fluid(maxWidth: 300) {
+						...GatsbyImageSharpFluid
+					}
+				}
+			}
+		}
+	`)
+
 	return (
 		<Layout>
 			<SEO title="Home" />
 			<IndexContainer>
-				<StaticImage style={{
-					borderRadius: '100%',
-					width: '75%',
-					height: 'auto',
-					margin: '0 auto',
-					boxShadow: '0 5px 5px rgba(0, 0, 0, 0.5)'
-				}}
-					className="avatar"
-					src="../assets/images/avatar.jpg"
-					alt="Avatar" />
+				<AvatarWrapper>
+					<Img fluid={data.placeholderImage.childImageSharp.fluid} />
+				</AvatarWrapper>
 				<Skills />
 				<Bio />
 			</IndexContainer>
