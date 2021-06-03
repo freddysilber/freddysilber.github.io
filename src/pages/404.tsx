73da75bd12a3c42af5cc5c2fd65a7bf97db59c1e
freddysilber <freddy.silber@gmail.com>
2021-06-02 19:35:56 -0600

    working on quote of the day for a 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,25 +1,24 @@
 import React from 'react';
 import axios from 'axios';
+import { Eclipse } from 'react-loading-io';
 
 import { Layout, SEO } from '../components';
 
 const NotFoundPage = () => {
 
 	const quoteOfTheDay = () => {
-		axios.get('https://quotes.rest/qod').then(quote => {
-			console.log(quote)
+		axios.get('https://quotes.rest/qod').then(({ data }) => {
+			console.log(data.contents.quotes);
+			return data.contents.quotes.map(({ quote }: any) => {
+				console.log(quote);
+				<p>{quote}</p>
+			})
 		}).catch(error => {
 			console.error(error)
-		})
-		// fetch('https://quotes.rest/qod').then(quote => {
-		// 	console.log(quote);
-		// }).catch(error => {
-		// 	console.log(error);
-		// });
+		});
 	};
 
-	const q = quoteOfTheDay();
-	console.log(q);
+	console.log(quoteOfTheDay());
 
 	return (
 		<Layout>
@@ -27,6 +26,8 @@ const NotFoundPage = () => {
 			<div className="has-text-centered">
 				<h1>404: Not Found</h1>
 				<hr />
+				{/* <Eclipse size={64} /> */}
+				{quoteOfTheDay()}
 				<p>You just hit a route that doesn't exist... the sadness.</p>
 			</div>
 		</Layout>
