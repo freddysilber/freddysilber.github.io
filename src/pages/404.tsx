97fd3ec4a543b4e65465b10952c72c40ceacda57
freddysilber <freddy.silber@gmail.com>
2021-06-02 18:35:52 -0600

    indentation

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -3,14 +3,14 @@ import React from 'react'
 import { Layout, SEO } from '../components'
 
 const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <div className="has-text-centered">
-      <h1>404: Not Found</h1>
-      <hr />
-      <p>You just hit a route that doesn't exist... the sadness.</p>
-    </div>
-  </Layout>
+	<Layout>
+		<SEO title="404: Not found" />
+		<div className="has-text-centered">
+			<h1>404: Not Found</h1>
+			<hr />
+			<p>You just hit a route that doesn't exist... the sadness.</p>
+		</div>
+	</Layout>
 )
 
 export default NotFoundPage
