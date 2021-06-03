74ef858763088e8099e36cf87dcb59c71a7664ea
freddysilber <freddy.silber@gmail.com>
2021-06-02 18:57:34 -0600

    new packages

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,16 +1,36 @@
-import React from 'react'
+import React from 'react';
+import axios from 'axios';
 
-import { Layout, SEO } from '../components'
+import { Layout, SEO } from '../components';
 
-const NotFoundPage = () => (
-	<Layout>
-		<SEO title="404: Not found" />
-		<div className="has-text-centered">
-			<h1>404: Not Found</h1>
-			<hr />
-			<p>You just hit a route that doesn't exist... the sadness.</p>
-		</div>
-	</Layout>
-)
+const NotFoundPage = () => {
+
+	const quoteOfTheDay = () => {
+		axios.get('https://quotes.rest/qod').then(quote => {
+			console.log(quote)
+		}).catch(error => {
+			console.error(error)
+		})
+		// fetch('https://quotes.rest/qod').then(quote => {
+		// 	console.log(quote);
+		// }).catch(error => {
+		// 	console.log(error);
+		// });
+	};
+
+	const q = quoteOfTheDay();
+	console.log(q);
+
+	return (
+		<Layout>
+			<SEO title="404: Not found" />
+			<div className="has-text-centered">
+				<h1>404: Not Found</h1>
+				<hr />
+				<p>You just hit a route that doesn't exist... the sadness.</p>
+			</div>
+		</Layout>
+	)
+}
 
 export default NotFoundPage
