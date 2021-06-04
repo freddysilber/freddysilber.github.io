310ecf27a1277ee3f86ef64ae87d90fa4f750393
freddysilber <freddy.silber@gmail.com>
2021-06-03 21:03:49 -0600

    ;

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,8 +1,12 @@
 import React, { useEffect } from 'react';
 import axios from 'axios';
+// @ts-ignore
 import { Eclipse } from 'react-loading-io';
 import { Layout, SEO } from '../components';
 
+/**
+ * Component that shows the 404 page when the user fucks up.
+ */
 const NotFoundPage = () => {
 	const [state, setState]: any = React.useState([]);
 	// Fetch quote of the day when user hits a 404 and fucks up
@@ -10,10 +14,13 @@ const NotFoundPage = () => {
 		axios.get('https://quotes.rest/qod').then(({ data }) => {
 			setState(data.contents.quotes);
 		}).catch(error => {
-			throw new Error(error);
+			setState([{ quote: 'Something is seriously wrong here...' }]);
+			console.error(error);
 		});
 	}, []);
-
+	/**
+	 * This function takes care of rendering the quote of the day and while loading, it will render a spinner
+	 */
 	const renderQuoteOfTheDay = () => {
 		if (state.length) {
 			return state.map((quote: any) => (
