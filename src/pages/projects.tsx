e7428c4760ce0540ee04a122831dc7f97f642aa4
freddysilber <freddy.silber@gmail.com>
2021-06-03 22:11:36 -0600

    adds project list

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -4,27 +4,39 @@ import {
 	SEO,
 } from '../components'
 import axios from 'axios';
+// @ts-ignore
+import { Eclipse } from 'react-loading-io';
 
 export default function ProjectsPage() {
 	const [state, setState] = useState([]);
 
 	useEffect(() => {
 		axios.get('https://api.github.com/users/freddysilber/repos').then((repos) => {
-			console.log(repos);
 			setState(repos.data);
 		}).catch(error => {
+			setState([]);
 			throw new Error(error);
 		})
-	}, [])
-	console.log(state);
+	}, []);
 
+	/**
+	 * Renders a list of Github repos... while loading, render a spinner
+	 */
 	const renderProjects = () => {
 		if (state.length) {
-			return (state.map((repo: any) => (
-				<a key={repo.id} href={repo.html_url}>{repo.name}</a>
-			)));
+			return (
+				<ol>
+					{state.map((repo: any) => {
+						return (
+							<li key={repo.id}>
+								<a href={repo.html_url as string} target="_blank">{repo.name}</a>
+							</li>
+						)
+					})}
+				</ol>
+			)
 		} else {
-			return <p>...</p>
+			return <Eclipse size={64} />
 		}
 	}
 
