3336e9475e155c1a67f831b291d764e512f99e68
freddysilber <freddy.silber@gmail.com>
2021-06-03 19:46:05 -0600

    quote of the day on 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,53 +1,39 @@
-import React from 'react';
-// import React, { useEffect } from 'react';
-// import axios from 'axios';
-// import { Eclipse } from 'react-loading-io';
-
+import React, { useEffect } from 'react';
+import axios from 'axios';
+import { Eclipse } from 'react-loading-io';
 import { Layout, SEO } from '../components';
 
-const NotFoundPage = (props: any) => {
-	// const [state, setState]: any = React.useState([]);
+const NotFoundPage = () => {
+	const [state, setState]: any = React.useState([]);
+	// Fetch quote of the day when user hits a 404 and fucks up
+	useEffect(() => {
+		axios.get('https://quotes.rest/qod').then(({ data }) => {
+			setState(data.contents.quotes);
+		}).catch(error => {
+			throw new Error(error);
+		});
+	}, []);
 
-	// useEffect(() => {
-	// 	console.log(state, setState)
-	// 	setState('test')
-	// 	// setState(Promise.resolve('test'))
-	// 	axios.get('https://quotes.rest/qod').then((data) => {
-	// 		console.log(data);
-	// 		setState(data);
-	// 	});
-	// });
-	// console.log(state)
-	// useEffect(() => {
-	// 	const getData = async () => {
-	// 		const response = await fetch('https://quotes.rest/qod');
-	// 		const json = await response.json()
-	// 		console.log(json);
-	// 		updateQuote(json);
-	// 	}
-	// 	getData();
-	// }, []);
-	// console.log(quote, updateQuote)
-
+	const renderQuoteOfTheDay = () => {
+		if (state.length) {
+			return state.map((quote: any) => (
+				<div key={quote}>
+					<p className="has-script-text is-size-3">{quote.quote}</p>
+					<p className="has-script-text is-size-4">- {quote.author}</p>
+				</div>
+			))
+		} else {
+			return <Eclipse size={64} />
+		}
+	}
 
 	return (
 		<Layout>
 			<SEO title="404: Not found" />
 			<div className="has-text-centered">
 				<h1>404: Not Found</h1>
-				<p>...</p>
-				<h3>AKA you fucked up</h3>
 				<hr />
-				{/* <Eclipse size={64} /> */}
-				{/* {quoteOfTheDay()} */}
-				{/* {quote.contents.quotes.map(quote => {
-					<p>quote</p>
-				})} */}
-				{/* <p>{quote?.contents.quotes[0].quotes}</p> */}
-				{/* {state} */}
-				{/* I WILL PUT A RANDOM QUOTE IN HERE FOR THE 404 */}
-				<p className="has-text-info">IDK where your trying to go, but you might need a web atlas for your journey...</p>
-				{/* <p>You just hit a route that doesn't exist... the sadness.</p> */}
+				{renderQuoteOfTheDay()}
 			</div>
 		</Layout>
 	)
