1933ec57295c54ad254c5cab8db154ee7d394065
Freddy Silber <freddy.silber@cloudcoach.com>
2022-07-29 23:57:16 -0600

    havent checked the site in a bit so lets see whats up...

--- src/components/Navigation/navigation.tsx
+++ src/components/Navigation/navigation.tsx
@@ -12,9 +12,9 @@ export default function Navigation(): JSX.Element {
 				<div className="navbar-start">
 					<Link className="navbar-item" to={Routes.home} activeClassName="active">Home</Link>
 					<Link className="navbar-item" to={Routes.blog} partiallyActive={true} activeClassName="active">Blog</Link>
-					{/* <Link className="navbar-item" to={Routes.projects} activeClassName="active">Projects</Link> */}
 					<Link className="navbar-item" to={Routes.resume} activeClassName="active">Resume</Link>
 					<Link className="navbar-item" to={Routes.credentials} activeClassName="active">Credentials</Link>
+					<Link className="navbar-item" to={Routes.projects} activeClassName="active">Projects</Link>
 				</div>
 			</div>
 		</nav>
