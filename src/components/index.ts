3b73a76d7f849a75e257f43005d22b179ab21bc9
Freddy Silber <freddy.silber@cloudcoach.com>
2022-07-31 22:17:11 -0600

    styling

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,6 +3,6 @@ export { default as Bio } from './bio';
 export { default as Skills } from './skills';
 export { default as Image } from './image';
 export { default as SEO } from './seo';
-export { default as SocialMedia } from './socialMedia';
+export { default as SocialMedia } from './SocialMedia/socialMedia';
 export { default as Navigation } from './Navigation/navigation';
 export { default as BlogSidebar } from './blogSidebar';
