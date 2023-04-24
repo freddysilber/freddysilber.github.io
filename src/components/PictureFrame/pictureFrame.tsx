00774ce85eb81a1d2c6c7677f7231255912cba02
Freddy Silber <freddy.silber@gmail.com>
2023-04-24 17:38:00 -0600

    fix: css fixes

--- src/components/PictureFrame/pictureFrame.tsx
+++ src/components/PictureFrame/pictureFrame.tsx
@@ -1,12 +1,22 @@
 import React, { PropsWithChildren, ReactNode } from 'react';
 import './pictureFrame.scss';
+import styled from 'styled-components';
+import breakpoints from '../../config/breakpoints';
+
+const FrameContainer = styled.div`
+	width: 50%;
+
+	@media(max-width: ${breakpoints.breakpointMd}) {
+		width: 100%;
+	}
+`;
 
 export default function PictureFrame(props: PropsWithChildren<ReactNode>) {
 	return (
-		<div className="frame-container">
+		<FrameContainer>
 			<div className="frames"	>
 				<div className="frame">{props.children}</div>
 			</div>
-		</div>
+		</FrameContainer>
 	);
 }
